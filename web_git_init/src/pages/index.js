6a89536276c8f6e5383704e9e35dd4f0daabe88b
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-01-08 12:53:22 -0800

    Update index.js

--- web_git_init/src/pages/index.js
+++ web_git_init/src/pages/index.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 import './styles.css';
 
-export default class ExampleCss extends Component {
-  constructor(props) {
-    super(props);
-    this.wrapperRef = React.createRef();
-  }
+// export default class ExampleCss extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.wrapperRef = React.createRef();
+//   }
 
-  handleClick() {
-    const wrapper = this.wrapperRef.current;
-    wrapper.classList.toggle('is-nav-open')
-  }
+//   handleClick() {
+//     const wrapper = this.wrapperRef.current;
+//     wrapper.classList.toggle('is-nav-open')
+//   }
 
-  render() {
-    return (
-      <div ref={this.wrapperRef} className="wrapper" >
-        <div className="nav">
-          <div className="nav__body">
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div ref={this.wrapperRef} className="wrapper" >
+//         <div className="nav">
+//           <div className="nav__body">
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 // const element =  />;
 
 // class MyHeader extends React.Component {
