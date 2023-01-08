0f37c1024bc86f380386387d49eaaab7ea94b61a
johnny.zheng@csedge.org <johnny.zheng@csedge.org>
2023-01-08 12:52:34 -0800

    changes

--- web_git_init/src/pages/communication/connectwithus.js
+++ web_git_init/src/pages/communication/connectwithus.js
@@ -5,7 +5,7 @@ function connectWithUs() {
     <div class="about-section">
   <h1>Connect With Us</h1>
   <p>Some text about who we are and what we do.</p>
-  <p>Resize the browser window to see that this page is responsive by the way.</p>
+  <p>Here is the team!</p>
     </div>
    
 
